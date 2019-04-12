f9c6d7602ffd6f0336d83bb0c892b2b2e62ba754
Yusuke Ito <novi.mad@gmail.com>
2019-04-12 17:28:26 +0900

    make Data.base64URLEncoded... internal func

--- Sources/CryptoKit/Utilities/Data+Base64URL.swift
+++ Sources/CryptoKit/Utilities/Data+Base64URL.swift
@@ -4,14 +4,14 @@ extension Data {
     /// Decodes a base64-url encoded string to data.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public init?(base64URLEncoded: String, options: Data.Base64DecodingOptions = []) {
+    internal init?(base64URLEncoded: String, options: Data.Base64DecodingOptions = []) {
         self.init(base64Encoded: base64URLEncoded.base64URLUnescaped(), options: options)
     }
     
     /// Decodes base64-url encoded data.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public init?(base64URLEncoded: Data, options: Data.Base64DecodingOptions = []) {
+    internal init?(base64URLEncoded: Data, options: Data.Base64DecodingOptions = []) {
         self.init(base64Encoded: base64URLEncoded.base64URLUnescaped(), options: options)
     }
     
@@ -21,7 +21,7 @@ extension Data {
     ///
     /// - parameter options: The options to use for the encoding. Default value is `[]`.
     /// - returns: The base64-url encoded string.
-    public func base64URLEncodedString(options: Data.Base64EncodingOptions = []) -> String {
+    internal func base64URLEncodedString(options: Data.Base64EncodingOptions = []) -> String {
         return base64EncodedString(options: options).base64URLEscaped()
     }
     
@@ -31,7 +31,7 @@ extension Data {
     ///
     /// - parameter options: The options to use for the encoding. Default value is `[]`.
     /// - returns: The base64-url encoded data.
-    public func base64URLEncodedData(options: Data.Base64EncodingOptions = []) -> Data {
+    internal func base64URLEncodedData(options: Data.Base64EncodingOptions = []) -> Data {
         return base64EncodedData(options: options).base64URLEscaped()
     }
 }
@@ -41,7 +41,7 @@ extension String {
     /// Converts a base64-url encoded string to a base64 encoded string.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public func base64URLUnescaped() -> String {
+    internal func base64URLUnescaped() -> String {
         let replaced = replacingOccurrences(of: "-", with: "+")
             .replacingOccurrences(of: "_", with: "/")
         /// https://stackoverflow.com/questions/43499651/decode-base64url-to-base64-swift
@@ -56,7 +56,7 @@ extension String {
     /// Converts a base64 encoded string to a base64-url encoded string.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public func base64URLEscaped() -> String {
+    internal func base64URLEscaped() -> String {
         return replacingOccurrences(of: "+", with: "-")
             .replacingOccurrences(of: "/", with: "_")
             .replacingOccurrences(of: "=", with: "")
@@ -65,14 +65,14 @@ extension String {
     /// Converts a base64-url encoded string to a base64 encoded string.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public mutating func base64URLUnescape() {
+    internal mutating func base64URLUnescape() {
         self = base64URLUnescaped()
     }
     
     /// Converts a base64 encoded string to a base64-url encoded string.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public mutating func base64URLEscape() {
+    internal mutating func base64URLEscape() {
         self = base64URLEscaped()
     }
 }
@@ -82,7 +82,7 @@ extension Data {
     /// Converts base64-url encoded data to a base64 encoded data.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public mutating func base64URLUnescape() {
+    internal mutating func base64URLUnescape() {
         for (i, byte) in enumerated() {
             switch byte {
             case 0x2D: self[i] = 0x2B
@@ -100,7 +100,7 @@ extension Data {
     /// Converts base64 encoded data to a base64-url encoded data.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public mutating func base64URLEscape() {
+    internal mutating func base64URLEscape() {
         for (i, byte) in enumerated() {
             switch byte {
             case 0x2B: self[i] = 0x2D
@@ -114,7 +114,7 @@ extension Data {
     /// Converts base64-url encoded data to a base64 encoded data.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public func base64URLUnescaped() -> Data {
+    internal func base64URLUnescaped() -> Data {
         var data = self
         data.base64URLUnescape()
         return data
@@ -123,7 +123,7 @@ extension Data {
     /// Converts base64 encoded data to a base64-url encoded data.
     ///
     /// https://tools.ietf.org/html/rfc4648#page-7
-    public func base64URLEscaped() -> Data {
+    internal func base64URLEscaped() -> Data {
         var data = self
         data.base64URLEscape()
         return data
